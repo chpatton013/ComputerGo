705c7c3338c446381342cafc95ceef0ced5d7ff4
Christopher Patton <chpatton013@gmail.com>
2014-10-17 10:55:50 -0700

    Add empty tests for score and successor calculation

---
+++ tests/base/state/score.cpp
@@ -0,0 +1,16 @@
+#include <catch.hpp>
+#include <cgo/base/state.hpp>
+
+using namespace cgo::base;
+
+TEST_CASE("base/state/score/empty") {
+}
+
+TEST_CASE("base/state/score/white") {
+}
+
+TEST_CASE("base/state/score/black") {
+}
+
+TEST_CASE("base/state/score/mixed") {
+}
---
+++ tests/base/state/successors.cpp
@@ -0,0 +1,16 @@
+#include <catch.hpp>
+#include <cgo/base/state.hpp>
+
+using namespace cgo::base;
+
+TEST_CASE("base/state/successors/empty") {
+}
+
+TEST_CASE("base/state/successors/white") {
+}
+
+TEST_CASE("base/state/successors/black") {
+}
+
+TEST_CASE("base/state/successors/mixed") {
+}
