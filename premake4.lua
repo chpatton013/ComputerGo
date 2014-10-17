ac12575b20ebd427cc177ea003c32f0bf9d8711a
Christopher Patton <chpatton013@gmail.com>
2014-10-17 10:54:47 -0700

    Add tests for state board consistency

--- premake4.lua
+++ premake4.lua
@@ -30,42 +30,52 @@ flags({'OptimizeSpeed'})
 
 project('cgo_base')
 kind('SharedLib')
-targetdir('./lib')
+targetdir('lib')
 files({'src/base/**.cpp', 'include/cgo/base/**.hpp', 'include/cgo/base/**.inl'})
 
 -- project('cgo_stdin')
 -- kind('SharedLib')
--- targetdir('./lib')
+-- targetdir('lib')
 -- files({'src/stdin/**.cpp', 'include/cgo/stdin/**.hpp', 'include/cgo/stdin/**.inl'})
 -- links({'cgo_base'})
 
 -- project('cgo_random')
 -- kind('SharedLib')
--- targetdir('./lib')
+-- targetdir('lib')
 -- files({'src/random/**.cpp', 'include/cgo/random/**.hpp', 'include/cgo/random/**.inl'})
 -- links({'cgo_base'})
 
 -- project('cgo_minimax')
 -- kind('SharedLib')
--- targetdir('./lib')
+-- targetdir('lib')
 -- files({'src/minimax/**.cpp', 'include/cgo/minimax/**.hpp', 'include/cgo/minimax/**.inl'})
 -- links({'cgo_base'})
 
 -- project('cgo_montecarlo')
 -- kind('SharedLib')
--- targetdir('./lib')
+-- targetdir('lib')
 -- files({'src/montecarlo/**.cpp', 'include/cgo/montecarlo/**.hpp', 'include/cgo/montecarlo/**.inl'})
 -- links({'cgo_base'})
 
 -- project('cgo_driver')
 -- kind('ConsoleApp')
--- targetdir('./bin')
+-- targetdir('bin')
 -- files({'src/driver/**.cpp', 'include/cgo/driver/**.hpp', 'include/cgo/driver/**.inl'})
--- links({'cgo_base', 'cgo_stdin', 'cgo_minimax', 'cgo_montecarlo'})
+-- links({
+--    'cgo_base',
+--    'cgo_stdin',
+--    'cgo_minimax',
+--    'cgo_montecarlo'
+-- })
 
--- project('cgo_test')
--- kind('ConsoleApp')
--- targetdir('./bin')
--- files({'tests/**.cpp', 'tests/**.hpp', 'tests/**.inl'})
--- includedirs({'./external/Catch/single_include'})
--- links({'cgo_base', 'cgo_stdin', 'cgo_minimax', 'cgo_montecarlo'})
+project('cgo_test')
+kind('ConsoleApp')
+targetdir('bin')
+files({'tests/**.cpp', 'tests/**.hpp', 'tests/**.inl'})
+includedirs({'external/Catch/single_include'})
+links({
+   'cgo_base',
+   -- 'cgo_stdin',
+   -- 'cgo_minimax',
+   -- 'cgo_montecarlo'
+})
---
+++ tests/base/state/board.cpp
@@ -0,0 +1,44 @@
+#include <catch.hpp>
+#include <cgo/base/state.hpp>
+
+using namespace cgo::base;
+
+TEST_CASE("base/state/board/homogeneous") {
+   State::Board emptyBoard;
+   State::Board whiteBoard;
+   State::Board blackBoard;
+
+   emptyBoard.fill(none);
+   whiteBoard.fill(white);
+   blackBoard.fill(black);
+
+   State emptyState(emptyBoard);
+   State whiteState(whiteBoard);
+   State blackState(blackBoard);
+
+   for (int row = 0; row < BOARD_DIMENSION; ++row) {
+      for (int col = 0; col < BOARD_DIMENSION; ++col) {
+         REQUIRE(emptyState.getMarker(Position(row, col)) == none);
+         REQUIRE(whiteState.getMarker(Position(row, col)) == white);
+         REQUIRE(blackState.getMarker(Position(row, col)) == black);
+      }
+   }
+}
+
+TEST_CASE("base/state/board/mixed") {
+   State::Board board;
+   board.fill(none);
+   for (int ndx = 0; ndx < BOARD_DIMENSION; ++ndx) {
+      board[ndx * BOARD_DIMENSION] = white;
+      board[ndx * BOARD_DIMENSION + BOARD_DIMENSION - 1] = black;
+   }
+
+   State mixedState(board);
+   for (int row = 0; row < BOARD_DIMENSION; ++row) {
+      REQUIRE(mixedState.getMarker(Position(row, 0)) == white);
+      REQUIRE(mixedState.getMarker(Position(row, BOARD_DIMENSION - 1)) == black);
+      for (int col = 1; col < BOARD_DIMENSION - 1; ++col) {
+         REQUIRE(mixedState.getMarker(Position(row, col)) == none);
+      }
+   }
+}
