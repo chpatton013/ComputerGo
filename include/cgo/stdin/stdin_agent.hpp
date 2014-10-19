357081f3f06d63b5bcb41071e89f41cf0c3168f0
Christopher Patton <chpatton013@gmail.com>
2014-10-18 19:29:35 -0700

    Add stdin agent

---
+++ include/cgo/stdin/stdin_agent.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cgo/base/agent.hpp>
+#include <cgo/base/state.hpp>
+
+namespace cgo {
+namespace stdin {
+
+class StdinAgent : public base::Agent {
+public:
+   StdinAgent(base::Marker marker);
+   virtual ~StdinAgent();
+
+   base::Move makeMove(base::State& state,
+    const std::tuple< base::Move, base::State >& predecessor);
+};
+
+} // namespace base
+} // namespace cgo
---
+++ src/stdin/stdin_agent.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <sstream>
+#include <cgo/stdin/stdin_agent.hpp>
+
+using namespace cgo::base;
+using namespace cgo::stdin;
+
+StdinAgent::StdinAgent(Marker marker) :
+   Agent(marker)
+{}
+
+/* virtual */ StdinAgent::~StdinAgent() {}
+
+Move StdinAgent::makeMove(State& state,
+ const std::tuple< Move, State >& predecessor) {
+   static const int bufferSize = 256;
+   char input[bufferSize];
+
+   Position position(-1, -1);
+
+   do {
+      std::cout << "Enter a move: <row> <col> or (p)ass: ";
+      std::cin.getline(input, bufferSize);
+
+      std::string move(input);
+
+      if (move == "p" || move == "pass") {
+         return Pass();
+      }
+
+      int row, col;
+      std::stringstream ss(move);
+      ss >> row >> col;
+
+      position = Position(row - 1, col -1);
+   } while (state.isActionValid(Action(this->_marker, position), predecessor));
+
+   return Action(this->_marker, position);
+}
