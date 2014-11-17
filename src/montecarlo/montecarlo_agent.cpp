3af2658e0da9db532d7ac378655cb75463495bf0
Matthew <matthewcp0@gmail.com>
2014-11-16 16:54:33 -0800

    src/montecarlo/montecarlo_agent.cpp

--- src/montecarlo/montecarlo_agent.cpp
+++ src/montecarlo/montecarlo_agent.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <cgo/montecarlo/montecarlo_agent.hpp>
+#include <cgo/montecarlo/montecarlo_tree.hpp>
 
 using namespace cgo::base;
 using namespace cgo::standardin;
@@ -14,6 +15,30 @@ MonteCarloAgent::MonteCarloAgent(Marker marker) :
 
 int MonteCarloAgent::CalculateBest(Position position) {
    return 0;
+}
+static const UCTK = sqrt(1/5);
+Node MonteCarloAgent::UCTSelect(Node& node) {
+   Node result = null;
+   Node next = node.child;
+
+   double best_uct = 0;
+   while (next != null) {
+      if (next.visits > 0) {
+         double winrate = next.getWinRate();
+         double uct = UCTK * sqrt(log(node.visits)/ next.visits);
+      }
+      else {
+         uctvalue = 10000 + 1000 * random();
+      }
+
+      if (uctvalue > best_uct) {
+         best_uct = uctvalue;
+         res = next;
+      }
+
+      next = next.sibling;
+   }
+   return res;
 }
 
 Move MonteCarloAgent::makeMove(State& state,
